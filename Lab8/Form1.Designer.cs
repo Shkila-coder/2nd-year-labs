153a042f1e186b846405c25be159b81e70e769f3
Shkila-coder <114057059+Shkila-coder@users.noreply.github.com>
2023-06-30 17:46:19 +0300

    Add files via upload

---
+++ Lab8/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Lab8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.graphWind = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.graphWind)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 32);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 0;
+            // 
+            // graphWind
+            // 
+            this.graphWind.BackColor = System.Drawing.Color.White;
+            this.graphWind.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.graphWind.Location = new System.Drawing.Point(597, 9);
+            this.graphWind.Name = "graphWind";
+            this.graphWind.Size = new System.Drawing.Size(389, 451);
+            this.graphWind.TabIndex = 1;
+            this.graphWind.TabStop = false;
+            this.graphWind.MouseClick += new System.Windows.Forms.MouseEventHandler(this.graphWind_MouseClick);
+            this.graphWind.MouseDown += new System.Windows.Forms.MouseEventHandler(this.graphWind_MouseDown);
+            this.graphWind.MouseMove += new System.Windows.Forms.MouseEventHandler(this.graphWind_MouseMove);
+            this.graphWind.MouseUp += new System.Windows.Forms.MouseEventHandler(this.graphWind_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(993, 467);
+            this.Controls.Add(this.graphWind);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            ((System.ComponentModel.ISupportInitialize)(this.graphWind)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox graphWind;
+    }
+}
+
---
+++ Lab8/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab8
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        bool MouseDown = false;
+        bool MouseUp = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            label1.Location = e.Location;
+            label1.Text = e.Button.ToString() + " " + e.Location.ToString();
+        }
+
+        private void graphWind_MouseClick(object sender, MouseEventArgs e)
+        {
+            if(MouseUp)
+            {
+                MouseUp = false;
+                return;
+            }
+            if (e.Button == MouseButtons.Right)
+                g.DrawLine(new Pen(Color.Black), 0, 0, e.Location.X, e.Location.Y);
+            if (e.Button == MouseButtons.Left)
+                g.DrawLine(new Pen(Color.Black), e.Location.X, e.Location.Y, graphWind.Width, graphWind.Height);
+        }
+
+        private void graphWind_MouseDown(object sender, MouseEventArgs e)
+        {
+            MouseDown = true;
+        }
+
+        private void graphWind_MouseUp(object sender, MouseEventArgs e)
+        {
+            MouseDown = false;
+        }
+
+        private void graphWind_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(MouseDown)
+            {
+                g.FillEllipse(new SolidBrush(Color.Black), e.Location.X-5, e.Location.Y-5, 10, 10);
+                MouseUp = true;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+                g = graphWind.CreateGraphics();
+                g.SmoothingMode = SmoothingMode.AntiAlias;
+        }
+    }
+}
